ccb39f984f761a51e1802a25d963cb707131a889
eidman <eidmanx@gmail.com>
2020-01-23 23:33:00 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,47 @@
+document.addEventListener("DOMContentLoaded", function() {
+  loadEvents();
+  // load every event in the page
+  let numberMax = 100;
+  let randomNumber = Math.ceil(Math.random() * 100);
+
+  function checkStatus(guessedNumber) {
+    if (isNaN(guessedNumber)) {
+      return `You must guess a number you fool!`;
+    } else if (guessedNumber < 1 || guessedNumber > numberMax) {
+      return `You must guess a number between 1 and 100`;
+    } else if (randomNumber > guessedNumber) {
+      return `The number you seek is greater than ${guessedNumber}`;
+    } else if (randomNumber < guessedNumber) {
+      return `The number you seek is less than ${guessedNumber}`;
+    } else if (guessedNumber == randomNumber) {
+      return `You win! ${guessedNumber} is the right number!`;
+    }
+  }
+
+  function loadEvents() {
+    document.querySelector("form").addEventListener("submit", submit);
+    document.getElementById("clear").addEventListener("click", clearList);
+  }
+
+  // NUMBER INPUT
+  function submit(event) {
+    event.preventDefault();
+    let guessedNumber = document.querySelector("input");
+    if (guessedNumber.value != "") addNumber(guessedNumber.value);
+    guessedNumber.value = "";
+  }
+
+  // ADD
+  function addNumber(number) {
+    let ul = document.querySelector("ul");
+    let li = document.createElement("li");
+    li.innerHTML = checkStatus(number);
+    ul.appendChild(li);
+    document.querySelector(".guesses").style.display = "block";
+  }
+
+  // CLEAR ALL
+  function clearList(event) {
+    document.querySelector("ul").innerHTML = "";
+  }
+});
