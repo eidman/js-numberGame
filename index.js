e828e197fef56d8f4630689087efbb47f460c6bf
eidman <eidmanx@gmail.com>
2020-01-29 18:39:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 document.addEventListener("DOMContentLoaded", function() {
   loadEvents();
   // load every event in the page
+  let guessCount = 0;
   let numberMax = 100;
-  let randomNumber = Math.ceil(Math.random() * 100);
+  let randomNumber = Math.ceil(Math.random() * numberMax);
 
   function checkStatus(guessedNumber) {
     if (isNaN(guessedNumber)) {
-      return `you must guess a number you fool!`;
+      return `you must guess a number!`;
     } else if (guessedNumber < 1 || guessedNumber > numberMax) {
       return `you must guess a number between 1 and 100!`;
     } else if (randomNumber > guessedNumber) {
       return `the number you seek is greater than ${guessedNumber}`;
     } else if (randomNumber < guessedNumber) {
       return `the number you seek is less than ${guessedNumber}`;
-    } else if (guessedNumber == randomNumber) {
+    } else if (guessedNumber === randomNumber) {
       return `you win! ${guessedNumber} is the right number!`;
     }
   }
@@ -27,15 +28,24 @@ document.addEventListener("DOMContentLoaded", function() {
   function submit(event) {
     event.preventDefault();
     let guessedNumber = document.querySelector("input");
-    if (guessedNumber.value != "") addNumber(guessedNumber.value);
+    let value = parseInt(guessedNumber.value);
+    let check = checkStatus(value);
+    guessCount++;
+    if (guessCount === 7 && guessedNumber != randomNumber) {
+      // show the you lose
+      addNumber("you lose");
+    } else if (value != "") addNumber(check);
+
     guessedNumber.value = "";
+
+    console.log(guessCount);
   }
 
   // ADD
-  function addNumber(number) {
+  function addNumber(text) {
     let ul = document.querySelector("ul");
     let li = document.createElement("li");
-    li.innerHTML = checkStatus(number);
+    li.innerHTML = text;
     ul.appendChild(li);
     document.querySelector(".guesses").style.display = "block";
   }
